283e024bb55bbacbf7ec681ac23fb75684f6b00a
陈宫域楠 <19hzheng1@stu.edu.cn>
2023-05-10 12:20:36 +0800

    "LAB 3 v3"

--- lab3/timingtest/AList.java
+++ lab3/timingtest/AList.java
@@ -1,18 +1,21 @@
 package timingtest;
 
-/** Array based list.
- *  @author Josh Hug
+/**
+ * Array based list.
+ * 
+ * @author Josh Hug
  */
 
-//         0 1  2 3 4 5 6 7
+// 0 1 2 3 4 5 6 7
 // items: [6 9 -1 2 0 0 0 0 ...]
 // size: 5
 
-/* Invariants:
- addLast: The next item we want to add, will go into position size
- getLast: The item we want to return is in position size - 1
- size: The number of items in the list should be size.
-*/
+/*
+ * Invariants:
+ * addLast: The next item we want to add, will go into position size
+ * getLast: The item we want to return is in position size - 1
+ * size: The number of items in the list should be size.
+ */
 
 public class AList<Item> {
     private Item[] items;
@@ -34,7 +37,7 @@ public class AList<Item> {
     /** Inserts X into the back of the list. */
     public void addLast(Item x) {
         if (size == items.length) {
-            resize(size + 1);
+            resize(size * 2);
         }
 
         items[size] = x;
@@ -45,6 +48,7 @@ public class AList<Item> {
     public Item getLast() {
         return items[size - 1];
     }
+
     /** Gets the ith item in the list (0 is the front). */
     public Item get(int i) {
         return items[i];
@@ -55,8 +59,10 @@ public class AList<Item> {
         return size;
     }
 
-    /** Deletes item from back of the list and
-      * returns deleted item. */
+    /**
+     * Deletes item from back of the list and
+     * returns deleted item.
+     */
     public Item removeLast() {
         Item x = getLast();
         items[size - 1] = null;
