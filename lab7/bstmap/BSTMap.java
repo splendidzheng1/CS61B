1f01fd09de4f6a9baffe7bede58936c99957e67c
陈宫域楠 <19hzheng1@stu.edu.cn>
2023-06-07 19:52:35 +0800

    Finish LAB7 with partial ex

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -2,15 +2,16 @@ package bstmap;
 
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
-public class BSTMap<K extends Comparable, V> implements Map61B{
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     /**
         BST Node imply data(key + value) reserved
         Every Node at the right is greater than this one
         Evert Node at the left is less than this one
      */
-    private class Node{
+    private class Node {
         K key;
         V value;
         Node left;
@@ -45,8 +46,8 @@ public class BSTMap<K extends Comparable, V> implements Map61B{
     }
 
     @Override
-    public boolean containsKey(Object key) {
-        return containsKey((K)key, root);
+    public boolean containsKey(K key) {
+        return containsKey(key, root);
     }
 
     private boolean containsKey(K key, Node node) {
@@ -56,7 +57,7 @@ public class BSTMap<K extends Comparable, V> implements Map61B{
         if (node.key.equals(key)) {
             return true;
         }
-        if (node.key.compareTo(key) > 0) {
+        if (node.key.compareTo(key) < 0) {
             return containsKey(key, node.right);
         } else {
             return containsKey(key, node.left);
@@ -64,8 +65,8 @@ public class BSTMap<K extends Comparable, V> implements Map61B{
     }
 
     @Override
-    public Object get(Object key) {
-        return get((K) key, root);
+    public V get(K key) {
+        return get(key, root);
     }
 
     private V get(K key, Node node) {
@@ -75,7 +76,7 @@ public class BSTMap<K extends Comparable, V> implements Map61B{
         if (node.key.equals(key)) {
             return node.value;
         }
-        if (node.key.compareTo(key) > 0) {
+        if (node.key.compareTo(key) < 0) {
             return get(key, node.right);
         } else {
             return get(key, node.left);
@@ -88,11 +89,11 @@ public class BSTMap<K extends Comparable, V> implements Map61B{
     }
 
     @Override
-    public void put(Object key, Object value) {
+    public void put(K key, V value) {
         if (root == null) {
-            root = new Node((K)key, (V)value);
+            root = new Node(key, value);
         } else {
-            put((K)key, (V)value, root);
+            put(key, value, root);
         }
         size += 1;
     }
@@ -101,7 +102,7 @@ public class BSTMap<K extends Comparable, V> implements Map61B{
         if (node == null) {
             return new Node(key, value);
         } else {
-            if (node.key.compareTo(key) >= 0) {
+            if (node.key.compareTo(key) < 0) {
                 node.right = put(key, value, node.right);
             } else {
                 node.left = put(key, value, node.left);
@@ -129,16 +130,87 @@ public class BSTMap<K extends Comparable, V> implements Map61B{
 
     @Override
     public Set keySet() {
-        throw new UnsupportedOperationException("Not support keySet yet");
+        return keySet(root);
+    }
+
+    private Set keySet(Node node) {
+        Set<K> res = new TreeSet<>();
+        keySet(res, node);
+        return res;
+    }
+
+    private void keySet(Set<K> res, Node node) {
+        if (node == null) {
+            return;
+        }
+        keySet(res, node.left);
+        res.add(node.key);
+        keySet(res, node.right);
+    }
+
+    private void substitudeWithRightLeftMostNode(Node node, Node parent) {
+        Node pre = node;
+        //  Find right-left most node
+        if (node.right == null) {
+            if (node.left == null) {
+                if (node == root) {
+                    root = null;
+                } else if (parent.left == node) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            } else {
+                node.key = node.left.key;
+                node.value = node.left.value;
+                node.right = node.left.right;
+                node.left = node.left.left;
+            }
+        } else {
+            Node cur = node.right;
+            while (cur.left != null) {
+                pre = cur;
+                cur = cur.left;
+            }
+            if (pre == node) {
+                cur.left = node.left;
+                node.key = cur.key;
+                node.value = cur.value;
+            } else {
+                cur.left = node.left;
+                cur.right  = node.right;
+                node.key = cur.key;
+                node.value = cur.value;
+                pre.left = null;
+            }
+        }
     }
 
     @Override
-    public Object remove(Object key) {
-        throw new UnsupportedOperationException("Not support remove yet");
+    public V remove(K key) {
+        Node node = root;
+        Node pre = null;
+        while (node != null) {
+            int cmp = node.key.compareTo(key);
+            if (cmp == 0) {
+                V res = node.value;
+                // substitute this node with its successor
+                substitudeWithRightLeftMostNode(node, pre);
+                size -= 1;
+                return res;
+            } else if (cmp > 0) {
+                pre = node;
+                node = node.left;
+            } else {
+                pre = node;
+                node = node.right;
+            }
+        }
+        return null;
     }
 
     @Override
-    public Object remove(Object key, Object value) {
+    public V remove(K key, V value) {
         throw new UnsupportedOperationException("Not support remove yet");
 
     }
